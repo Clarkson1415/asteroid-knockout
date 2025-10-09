3981d837b2b0b7d8d8160d0e4d5f84cf7b1a2f23
Clarkson <graceclarkson1415@gmail.com>
2025-10-09 19:28:08 +1000

    highscore animated

--- scripts/HighScoreLabel.cs
+++ scripts/HighScoreLabel.cs
@@ -5,6 +5,25 @@ public partial class HighScoreLabel : Label
 {
 	public void UpdateHighScoreText()
 	{
-		Text = Level.GetHighScore().ToString();
+		var highScore = Level.GetHighScore();
+		var timeForAnimation = 1f;
+		var timer = new Timer();
+		AddChild(timer);
+		timer.WaitTime = timeForAnimation / highScore;
+
+		var number = 0;
+		timer.Timeout += () => 
+		{
+			if (number == highScore)
+			{
+                timer.Stop(); // IDK if this means the timer will stop replaying if stop() is called and its not a one shot.
+                return;
+			}
+
+			Text = number.ToString(); 
+			number++; 
+		};
+
+		timer.Start();
     }
 }
